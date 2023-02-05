3d8cd178f78823d5c3242c82a3f77b2c25b94250
Ricardo Jesus Malagon Jerez <13302853+rjmalagon@users.noreply.github.com>
2023-02-05 03:09:37 +0000

    Update efirma-cambia-password-gui.sh

--- efirma-cambia-password-gui.sh
+++ efirma-cambia-password-gui.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 ARCHIVO=$(zenity --info --text 'Seleccionar archivo key de efirma para remplazo de clave' && zenity --title 'Seleccionar archivo key de efirma' --file-selection) KEYPASSWORD=$(z>
