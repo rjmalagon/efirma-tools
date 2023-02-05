f5ded4da1314c485308698284b2f01fe49b72f92
Ricardo Jesus Malagon Jerez <13302853+rjmalagon@users.noreply.github.com>
2023-02-05 03:10:37 +0000

    Update efirma-cambia-password.sh

--- efirma-cambia-password.sh
+++ efirma-cambia-password.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 openssl pkcs8 -topk8 -in $1 -out $1-nuevaclave.key -v2prf hmacWithSHA1 -v2 des-ede3-cbc -iter 2048  -outform DER
 echo "Se ha creado archivo con nueva contraseña en" $1-nuevaclave.key
